cac1f8a6cf12b4d71035557e4a7491823ce1a2b6
George <georgewang89@gmail.com>
2015-11-12 16:22:26 -0500

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var spark = require('spark');
-var twilio = require('twilio.js')
+var twilio = require('./twilio.js')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,9 +12,26 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/',function(request, response){
-response.send('main page works!');
+response.render('./pages/home.ejs')
+});
 
 app.get('/sendSMS', function(request, response){
   twilio.sendSMS();
+  console.log('hello');
   response.send('SMS sent!');
+});
+
+app.get('/respondtotwilliosms', function(request, respond)
+  if (twilio.validateExpressRequest(req, 'YOUR_AUTH_TOKEN')) {
+       var twiml = new twilio.TwimlResponse();
+       console.log(twiml);
+       twilio.sendSMS();
+  }
+});
+
+console.log(
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
