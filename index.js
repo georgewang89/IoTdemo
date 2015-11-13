a226fea88a732f35290ac2429b109285222453f6
George <georgewang89@gmail.com>
2015-11-13 15:25:53 -0500

    heroku

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
