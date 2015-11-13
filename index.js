d33854a751ab906edafdcad7b788ff1ef69f7062
George <georgewang89@gmail.com>
2015-11-12 22:39:32 -0500

    heroku

--- index.js
+++ index.js
@@ -11,24 +11,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/',function(request, response){
-  //response.send('hello');
   response.render('pages/home');
 });
-/*
 
 app.get('/sendSMS', function(request, response){
   twilio.sendSMS();
   console.log('hello');
   response.send('SMS sent!');
 });
-/*
+
 app.get('/respondtotwilliosms', function(request, respond){
   if (twilio.validateExpressRequest(req, '143c81738b3c5ef61b3652b27e9400b4')) {
        var twiml = new twilio.TwimlResponse();
        console.log(twiml);
        twilio.sendSMS();
   }
-});*/
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
