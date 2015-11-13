107b9e9304fa31dc24395af574b03936ad1bafaf
George <georgewang89@gmail.com>
2015-11-12 23:49:35 -0500

    heroku

--- index.js
+++ index.js
@@ -20,13 +20,12 @@ app.get('/sendSMS', function(request, response){
   response.send('SMS sent!');
 });
 
-app.get('/respondtotwiliosms', function(request, response){
+app.post('/respondtotwiliosms', function(request, response){
   if (twilio.validateExpressRequest(req, '143c81738b3c5ef61b3652b27e9400b4')) {
        var twiml = new twilio.TwimlResponse();
 
        twilio.sendSMS();
        response.send('');
-     }
 });
 
 app.listen(app.get('port'), function() {
