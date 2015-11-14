971c5301257f3d1039dac2cdd91aa7409c5835f2
George <georgewang89@gmail.com>
2015-11-13 23:15:37 -0500

    heroku

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var twilio = require('./twilio.js');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 //var ejs = require('ejs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-/*
-app.use(bodyParser.json() );       // to support JSON-encoded bodies
+//app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+  extended: false
 }));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
