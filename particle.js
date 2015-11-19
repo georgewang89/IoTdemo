71f382da9b660a39356a88fe8e0e004bcab03e9a
George <georgewang89@gmail.com>
2015-11-19 16:58:58 -0500

    init

--- particle.js
+++ particle.js
@@ -6,24 +6,33 @@ module.exports = {
   createEvent: function (test){
 
     console.log('hello!!!!');
+
     var accessToken = "d90d64dbf20b94969e33c40c02a7bbd565244e48"
-    var deviceID = "390036001347343432313031";
-    var setFunc = "twiliofunc";
-    var requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + setFunc + "/";
+    var deviceID = "3a0029000b47343138333038";
+    var setFunc = "motorset";
+    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/" + setFunc;
 
+    //$.post('https://api.particle.io/v1/devices/3a0029000b47343138333038/motorset', { arg: 'on', access_token: 'accessToken' });
 
-
-    $.post( requestURL, { params: "aa", access_token: accessToken });
-
-
+    $.ajax({
+      url: 'https://api.particle.io/v1/devices/390036001347343432313031/motorset',
+      type: 'post',
+      data: {
+          arg: 'on'
+      },
+      headers: {
+          Authorization: 'Bearer '+ accessToken
+      },
+      dataType: 'application/json'
+    });
   }
 };
-
+/*
 var accessToken = "d90d64dbf20b94969e33c40c02a7bbd565244e48"
 var deviceID = "390036001347343432313031";
 var setFunc = "twiliofunc";
 var requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + setFunc + "/";
-/*
+
 function activateFunction(){
 $.ajax({
   type: "POST",
